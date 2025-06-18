f282cae58987857c5d0fc33ac4371795ffe3db7a
EugeneWinter <corvoattano200529@gmail.com>
2025-06-19 01:20:07 +0300

    29

--- init-db.sh
+++ init-db.sh
@@ -2,8 +2,8 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    DROP TABLE IF EXISTS url_checks;
-    DROP TABLE IF EXISTS urls;
+    DROP TABLE IF NOT EXISTS url_checks;
+    DROP TABLE IF NOT EXISTS urls;
 
     CREATE TABLE urls (
         id BIGSERIAL PRIMARY KEY,
